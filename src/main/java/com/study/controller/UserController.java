17c032c77216bd9aec1ecebb24fbc0f9244cf57b
keykeyshome <keykeyshome@126.com>
2021-08-24 00:47:08 +0800

    热部署复习 及 mybatis plus 使用学习

--- src/main/java/com/study/controller/UserController.java
+++ src/main/java/com/study/controller/UserController.java
@@ -1,6 +1,9 @@
 package com.study.controller;
 
 
+import com.study.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
@@ -16,5 +19,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/user")
 public class UserController {
+    @Autowired
+    UserService userService;
+    @GetMapping("index")
+    public Object index(){
+        return userService.getById(1L);
+    }
 
 }
